360647a6b056d735487dc93909d715d55440c8d3
Tony Nguyen <thnguyen1215@gmail.com>
2017-04-28 10:43:17 -0500

    multiple permissions
    multiple permissions

--- app/src/main/java/ku/eecscap/team8/auricle/Permission.java
+++ app/src/main/java/ku/eecscap/team8/auricle/Permission.java
@@ -4,6 +4,7 @@ package ku.eecscap.team8.auricle;
  * Created by thngu on 4/4/2017.
  */
 
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,14 +15,136 @@ import android.util.Log;
 import android.support.v4.app.ActivityCompat;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
-//ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, 101);
+import android.widget.Toast;
+
 public class Permission extends AppCompatActivity {
 
-    private static String TAG = "Auricle";
-    public static final int PERMISSIONS_MULTIPLE_REQUEST = 123;
+   // private static String TAG = "Auricle";
+   // public static final int PERMISSIONS_MULTIPLE_REQUEST = 123;
    // private static final int RECORD_REQUEST_CODE = 101;
+   // private static final int REQUEST_RECORD_AUDIO = 110;
+   // private static final int REQUEST_MODIFY_AUDIO_SETTINGS = 111;
+   // private static final int REQUEST_READ_EXTERNAL_STORAGE = 112;
+   // private static final int REQUEST_WRITE_EXTERNAL_STORAGE = 113;
+    Context context = getApplicationContext();
+    CharSequence text = "";
+    int duration = Toast.LENGTH_SHORT;
+    Toast Toast = Toast.makeText(context, text, duration);
+    int PERMISSIONS_ALL = 1;
+    String[] PERMISSIONS = {Manifest.permission.RECORD_AUDIO,
+                            Manifest.permission.MODIFY_AUDIO_SETTINGS,
+                            Manifest.permission.READ_EXTERNAL_STORAGE,
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE};*/
+
+    public void askPermission()
+    {
+        for (String permission : PERMISSIONS) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(PERMISSIONS, PERMISSIONS_ALL);
+                return;
+            }
+        }
+    }
 
     @Override
+    public void onRequestPermissionsResult (int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case 1:{
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                    //Do your work.
+                } else {
+                    Toast.makeText(this, "Until you grant the permission, we cannot proceed further", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+}
+    /*@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        boolean hasPermissionRecordAudio = (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.RECORD_AUDIO) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermissionRecordAudio) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    REQUEST_RECORD_AUDIO);
+        }
+
+        boolean hasPermissionModifyAudio = (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.MODIFY_AUDIO_SETTINGS) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermissionModifyAudio) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.MODIFY_AUDIO_SETTINGS},
+                    REQUEST_MODIFY_AUDIO_SETTINGS);
+        }
+
+        boolean hasPermissionWrite = (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermissionWrite) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    REQUEST_WRITE_EXTERNAL_STORAGE);
+        }
+
+        boolean hasPermissionRead = (ContextCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED);
+        if (!hasPermissionRead) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                    REQUEST_READ_EXTERNAL_STORAGE);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_AUDIO: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show();
+                    //reload my activity with permission granted or use the features what required the permission
+                    finish();
+                    startActivity(getIntent());
+                } else {
+                    Toast.makeText(this, "The app was not allowed to get your phone state. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
+                }
+            }
+            case REQUEST_MODIFY_AUDIO_SETTINGS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show();
+                    //reload my activity with permission granted or use the features what required the permission
+                    finish();
+                    startActivity(getIntent());
+                } else {
+                    Toast.makeText(this, "The app was not allowed to get your location. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
+                }
+            }
+            case REQUEST_WRITE_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show();
+                    //reload my activity with permission granted or use the features what required the permission
+                    finish();
+                    startActivity(getIntent());
+                } else {
+                    Toast.makeText(this, "The app was not allowed to write to your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            case REQUEST_READ_EXTERNAL_STORAGE: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show();
+                    //reload my activity with permission granted or use the features what required the permission
+                    finish();
+                    startActivity(getIntent());
+                } else {
+                    Toast.makeText(this, "The app was not allowed to read your storage. Hence, it cannot function properly. Please consider granting it this permission", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+}*/
+
+    /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.request_permission);
@@ -80,4 +203,4 @@ public class Permission extends AppCompatActivity {
             }
         }
     }
-}
+}*/
